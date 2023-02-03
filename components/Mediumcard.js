c9d322170c14b0f4ffc2d903eb50f63e6ab7d26f
0xNunana <kudayapaul@gmail.com>
2023-02-03 18:08:36 +0000

    Medium size

--- components/Mediumcard.js
+++ components/Mediumcard.js
@@ -1,9 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Mediumcard = () => {
+const Mediumcard = ({img,view}) => {
   return (
-    <div>
-      partners
+    <div className='hover:scale-110'>
+      <div className='relative'>
+        <Image src={img} height={10} width={250} alt="views" object-fit="contain"/>
+      </div>
+      <div className='text-center'>
+        <p>{view}</p>
+      </div>
     </div>
   )
 }
