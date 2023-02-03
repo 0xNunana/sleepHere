de72a058ad5c8a3c817731af5a21c00e09e5b3f1
0xNunana <kudayapaul@gmail.com>
2023-02-03 18:07:16 +0000

    Large image

--- components/Largecard.js
+++ components/Largecard.js
@@ -1,10 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Largecard = () => {
   return (
-    <div>
-      Large cards
-    </div>
+      <div className='relative '>
+        <Image alt="Lcard" className="rounded-xl" src="https://visitghana.com/wp-content/uploads/2019/05/4168_JamestownLightHouse1.jpg" height={10} width ={2000}/>
+      </div>
   )
 }
 
