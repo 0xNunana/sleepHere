ac4ee906d31a6fd7c09e4b29c3769b610a5e2254
0xNunana <kudayapaul@gmail.com>
2023-02-03 18:07:58 +0000

    small images

--- components/Smallcard.js
+++ components/Smallcard.js
@@ -3,11 +3,13 @@ import React from 'react'
 
 const Smallcard = ({img,location,distance}) => {
   return (
-    <div className='flex'>
-      <Image src={img} height={10} width={100} alt="smallie"/>
-      <div>
-        {location}
-        {distance}
+    <div className='flex items-center hover:scale-110'>
+      <div className='pb-2'> 
+        <Image className='rounded-md' src={img} height={10} width={120} alt="smallie"/>
+        </div>
+      <div className='pl-2 '>
+        <p>{location}</p>
+        <p>{distance}</p>
       </div>
     </div>
 
