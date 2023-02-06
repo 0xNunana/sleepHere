c3fc8d898d1da01b6f0a18f4d3af4a5adbe570ec
0xNunana <kudayapaul@gmail.com>
2023-02-06 20:27:07 +0000

    Map env

--- next.config.js
+++ next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
     "thumbs.dreamstime.com","static.tacdn.com","i0.wp.com","twellium.com","floorspacerealty.com","img.freepik.com","www.theechohouse.com","cdn.modernghana.com","www.myjoyonline.com","media-cdn.tripadvisor.com","citinewsroom.com","res.cloudinary.com"
   ]
   },
+  env:{
+    map_key:"pk.eyJ1IjoiMHhudW5hbmEiLCJhIjoiY2xkc29sZDRjMHVxeTN4cXNvMmJuamd4aSJ9.NLo83bhzfhqz1ue9q3Nc5w",
+  },
   reactStrictMode: true,
 }
 
