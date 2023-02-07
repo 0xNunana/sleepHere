15e73e16653cc915b3097d4200b215067f3c40b1
0xNunana <kudayapaul@gmail.com>
2023-02-07 12:20:20 +0000

    Redux store provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import {Provider} from "react-redux"
+import {store} from "../app/store"
 
 export default function App({ Component, pageProps:{session,...pageProps} }) {
   return (<SessionProvider session={session}>
-    <Component {...pageProps} />
+    <Provider store={store}> <Component {...pageProps} />
+    </Provider>
     </SessionProvider>)
 }
